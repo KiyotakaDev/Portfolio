3e8bef5c680f32edb23aee32e81fcb7a904fdbbb
きよたか <c4mo.sw@gmail.com>
2023-12-13 19:57:16 -0500

    feat(App.jsx): Using NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React, { lazy, Suspense } from 'react'
-import PrincipalLoader from './loaders/PrincipalLoader'
+import React, { lazy, Suspense } from "react";
+import PrincipalLoader from "./loaders/PrincipalLoader";
 
-
-const Scene = lazy(() => import("./canvas/Scene"))
+const Scene = lazy(() => import("./canvas/Scene"));
+const NavBar = lazy(() => import("./components/NavBar"));
 
 const App = () => {
   return (
     <Suspense fallback={<PrincipalLoader />}>
       <Scene />
+      <NavBar />
     </Suspense>
-  )
-}
+  );
+};
 
-export default App
+export default App;
