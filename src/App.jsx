a3b5cdb361a52e824a52a5166dd2b9660982780e
きよたか <c4mo.sw@gmail.com>
2023-12-14 10:02:46 -0500

    style(App.jsx): Adding frags

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import PrincipalLoader from "./loaders/PrincipalLoader";
 
 const Scene = lazy(() => import("./canvas/Scene"));
 const NavBar = lazy(() => import("./components/NavBar"));
-const Head = lazy(() => import("./components/Head"))
+const Head = lazy(() => import("./components/Head"));
 
 const App = () => {
   return (
     <>
-    {/* Principal content */}
-    <Suspense fallback={<PrincipalLoader />}>
-      <Scene />
-      <NavBar />
-      <Head />
-    </Suspense>
+      {/* Principal content */}
+      <Suspense fallback={<PrincipalLoader />}>
+        <Scene />
+        <NavBar />
+        <Head />
+      </Suspense>
     </>
   );
 };
