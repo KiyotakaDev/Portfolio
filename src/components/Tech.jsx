d7ecf4ff22f7213c17650d0aa2e55931b505a8b9
きよたか <c4mo.sw@gmail.com>
2023-12-14 17:15:02 -0500

    feat(Tech.jsx): Adding styles to the ball canvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,12 +1,32 @@
-import React, { lazy } from "react";
+import React, { lazy, useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { styles, motions } from '../utils'
+import { styles, motions } from "../utils";
 import { technologies } from "../constants";
 
-const BallCanvas = lazy(() => import("../canvas/BallCanvas"))
+const BallCanvas = lazy(() => import("../canvas/BallCanvas"));
 
 // Rendering the technologies
 const Tech = () => {
+  const ballSize =
+    "h-12 s:h-16 ls:w-24 sm:h-[6.5rem] md:h-[6.5rem] 1k:h-[10.3rem] 2k:h-[12.2rem] 4k:h-[20.5rem] w-12 s:w-16 ls:w-24 sm:w-[6.5rem] md:w-30 1k:w-[10.3rem] 2k:w-[12.2rem] 4k:w-[20.5rem]";
+  const ballGap = "gap-0 s:gap-2 md:gap-4 xl:gap-x-16 2k:gap-x-10 4k:gap-2"
+
+  const [icons, setIcons] = useState([]);
+
+  useEffect(() => {
+    const loadIcons = async () => {
+      try {
+        const iconPromises = technologies.map((tech) => tech.icon());
+        const loadedIcons = await Promise.all(iconPromises);
+        setIcons(loadedIcons);
+      } catch (error) {
+        console.error("Icons load error: ", error);
+      }
+    };
+
+    loadIcons();
+  }, []);
+
   return (
     <>
       <motion.p
@@ -15,11 +35,13 @@ const Tech = () => {
       >
         Skills
       </motion.p>
-      {technologies.map((technology, index) => (
-        <div key={index}>
-          <BallCanvas technology={technology} />
-        </div>
-      ))}
+      <div className={`${ballGap} flex flex-wrap justify-center items-center`}>
+        {icons.map((icon, index) => (
+          <div key={index} className={`${ballSize}`}>
+            <BallCanvas techIcon={icon} />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
