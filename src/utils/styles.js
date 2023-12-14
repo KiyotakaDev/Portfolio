f5479d6abc195f0347fe37bdad6e9cad426d3891
きよたか <c4mo.sw@gmail.com>
2023-12-13 19:55:18 -0500

    feat(styles.js): Creating reusable styles

---
+++ src/utils/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+  // Nav styless
+  navPadding: "p-5 sm:p-6 2k:px-20 4k:px-24 2k:py-10 4k:py-12",
+  portfolioImg: "h-12 xl:h-16 2k:h-20 4k:h-28 w-12 xl:w-16 2k:w-20 4k:w-28",
+  portfolioMainText:
+    "text-3xl s:text-4xl sm:text-5xl xl:text-6xl 2k:text-8xl 4k:text-9xl",
+  portfolioLinkText:
+    "text-2xl xs:text-3xl sm:text-4xl xl:text-5xl 2k:text-7xl 4k:text-8xl",
+}
+
+export { styles }
