e62c68e3d6800fc2bd0a0578fe3796ed10aba499
きよたか <c4mo.sw@gmail.com>
2023-12-14 10:49:27 -0500

    style(styles.js): Changing font-weight

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -3,9 +3,9 @@ const styles = {
   navPadding: "p-5 sm:p-6 2k:px-20 4k:px-24 2k:py-10 4k:py-12",
   portfolioImg: "h-12 xl:h-16 2k:h-20 4k:h-28 w-12 xl:w-16 2k:w-20 4k:w-28",
   portfolioMainText:
-    "text-3xl s:text-4xl sm:text-5xl xl:text-6xl 2k:text-8xl 4k:text-9xl",
+    "font-semibold text-3xl s:text-4xl sm:text-5xl xl:text-6xl 2k:text-8xl 4k:text-9xl",
   portfolioSubText:
-    "font-bold text-xl s:text-2xl sm:text-3xl md:text-4xl 2k:text-7xl 4k:text-8xl lg:leading-[2.5rem]",
+    "text-xl s:text-2xl sm:text-3xl md:text-4xl 2k:text-7xl 4k:text-8xl lg:leading-[2.5rem]",
   portfolioLinkText:
     "text-2xl xs:text-3xl sm:text-4xl xl:text-5xl 2k:text-7xl 4k:text-8xl",
 };
