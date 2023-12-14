f7be0f75d5c80801224784a69708eb6d6f7a06a7
きよたか <c4mo.sw@gmail.com>
2023-12-14 13:44:06 -0500

    feat(technologies): Adding the rest of the images

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,17 @@ const navLinks = [
 
 const technologies = [
   { name: "HTML5", icon: () => import('/tech/html.png')},
-  { name: "CSS3", icon: () => import('/tech/css.png')}
+  { name: "CSS3", icon: () => import('/tech/css.png')},
+  { name: "JavaScript", icon: () => import('/tech/javascript.png')},
+  { name: "TailwindCSS", icon: () => import('/tech/tailwindcss.png')},
+  { name: "ReactJS", icon: () => import('/tech/reactjs.png')},
+  { name: "ThreeJS", icon: () => import('/tech/threejs.png')},
+  { name: "Zustand", icon: () => import('/tech/zustand.png')},
+  { name: "NodeJS", icon: () => import('/tech/nodejs.png')},
+  { name: "ExpressJS", icon: () => import('/tech/expressjs.png')},
+  { name: "MongoDB", icon: () => import('/tech/mongodb.png')},
+  { name: "Git", icon: () => import('/tech/git.png')},
+  { name: "C#", icon: () => import('/tech/csharp.png')},
 ]
 
 export { navLinks, technologies }
