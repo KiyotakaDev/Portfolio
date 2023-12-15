b9489915388076f35ac1c9ee879054c3b0f3669f
きよたか <c4mo.sw@gmail.com>
2023-12-15 18:55:07 -0500

    style(PrincipalLoader.jsx): Changing height vh to %

--- src/loaders/PrincipalLoader.jsx
+++ src/loaders/PrincipalLoader.jsx
@@ -3,7 +3,7 @@ import "ldrs/bouncy";
 const PrincipalLoader = () => {
   return (
     <div
-      className="h-screen w-full flex justify-center items-center bg-leveled-950"
+      className="h-full w-full flex justify-center items-center bg-leveled-950"
     >
       <l-bouncy size="250" speed="1.75" color="white" />
     </div>
