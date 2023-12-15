db8c4e7fba64e3ca61b8199dcad8f42d8b2368e0
きよたか <c4mo.sw@gmail.com>
2023-12-14 21:53:46 -0500

    feat(index.js): Adding experience work data

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,4 +19,47 @@ const technologies = [
   { name: "C#", icon: () => import("/tech/csharp.png") },
 ];
 
-export { navLinks, technologies };
+const experiences = [
+  {
+    title: "PokeAPI",
+    company_name: "Personal experience",
+    icon: () => import("/experience/poke.png"),
+    iconBg: "#202020",
+    date: "September 2022 - November 2022",
+    points: [
+      "Querying data with async and await along with fecth",
+      "Quering data with .then() along with fetch",
+      "Data mapping for subsequent sampling",
+      "Routing usage",
+    ],
+  },
+  {
+    title: "Application documenter",
+    company_name: "Claro",
+    icon: () => import("/companies/claro.png"),
+    iconBg: "#fff",
+    date: "November 2022 - October 2023",
+    points: [
+      "In my intership whit the company they assigned me to document applications",
+      "Teamwork if there were problems with any application",
+      "Make queries to a database for subsequent sampling",
+    ],
+  },
+  {
+    title: "Chappy",
+    company_name: "Personal experience",
+    icon: () => import("/experience/chappy.png"),
+    iconBg: "#202020",
+    date: "January 2023 - April 2023",
+    points: [
+      "User authentication",
+      "API REST",
+      "Use of MERN",
+      "More complex routing",
+      "Use of tokens for application and data security",
+      "Use of sockets for real-time chat",
+    ],
+  },
+];
+
+export { navLinks, technologies, experiences };
