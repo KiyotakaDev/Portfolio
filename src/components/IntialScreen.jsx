a1947e630fa734ea8c2eeca1acc358e54953a4fc
きよたか <c4mo.sw@gmail.com>
2023-12-15 19:05:23 -0500

    style(InitialScreen.jsx): Adding padding to the component

--- src/components/IntialScreen.jsx
+++ src/components/IntialScreen.jsx
@@ -8,9 +8,9 @@ const InitialScreen = () => {
         opacity: [1, 1, 1, 1, 0],
       }}
       transition={{ duration: 5, ease: "easeIn" }}
-      className="text-5xl md:text-8xl 4k:text-9xl fixed h-screen w-full bg-leveled-700 z-50 flex flex-col justify-center items-center text-white neon-shadow"
+      className="h-screen w-full text-5xl md:text-8xl 4k:text-9xl bg-leveled-700 z-50 fixed flex flex-col justify-center items-center px-10 text-white neon-shadow"
     >
-      <p className="font-extrabold tracking-widest mb-10">
+      <p className="font-extrabold tracking-widest mb-10 text-center">
         Welcome to my portfolio
       </p>
       {/* Bounce effect */}
